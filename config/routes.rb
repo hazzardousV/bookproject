284dae3df8718a68684a3ab4ec9dc69759e645fb
HazzardousV <quinweidner@gmail.com>
2016-12-23 10:39:29 +0100

    bug 2 with routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  rout to: 'books#index'
+  root to: 'books#index'
   resources :books
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
